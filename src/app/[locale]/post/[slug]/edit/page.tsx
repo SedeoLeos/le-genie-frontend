9c628162ca434e3964d04be8ce5a979993ec2859
Slaega <smatsoula19@gmail.com>
2025-06-03 23:37:33 +0100

    post page

--- src/app/[locale]/post/[slug]/edit/page.tsx
+++ src/app/[locale]/post/[slug]/edit/page.tsx
@@ -1,6 +1,18 @@
 import { ContentManager } from "@/components/tiptap-templates/simple/simple-editor"
-
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 export default function EditPost() {
-  return <ContentManager content={""} viewer={false} />
+  return <div className="flex flex-col gap-5">
+    <div className="flex justify-end">
+
+      <Button variant={'ghost'}>Sauvegarder</Button>
+    </div>
+    <div className="h-40 w-full bg-gray-400 border-2 border-gray-600 rounded-lg cursor-pointer">
+
+      <Input type="file"  className="hideen"/>
+    </div>
+    <Input />
+    <ContentManager content={""} viewer={false} />
+  </div>
 }
